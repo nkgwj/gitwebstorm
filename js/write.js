741c9dd4e8036184f8a69c13a22df790ef2ea1ae
Katsuyuki Nakagawaji <k-nkgwj@ics.es.osaka-u.ac.jp>
2013-01-13 00:30:53 +0900

    first commit

---
+++ js/write.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: k-nkgwj
+ * Date: 13/01/13
+ * Time: 0:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+document.write("hoge");
